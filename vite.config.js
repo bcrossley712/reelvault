3f352e40c990b4c608ff419dc9f86dd03b5868a1
bcrossley712 <bcrossley712@gmail.com>
2026-05-22 17:53:09 -0600

    Added iphone downloadability

--- vite.config.js
+++ vite.config.js
@@ -18,43 +18,35 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait',
         icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
       workbox: {
-  globPatterns: ['**/*.{js,css,html,ico,png,svg,xlsx}'],
-  // Force xlsx to always be re-validated against the server
-  runtimeCaching: [
-    {
-      urlPattern: /movies\.xlsx$/,
-      handler: 'NetworkFirst',  // try network first, fall back to cache
-      options: {
-        cacheName: 'data-cache',
-        networkTimeoutSeconds: 5,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,xlsx}'],
+        runtimeCaching: [
+          {
+            urlPattern: /movies\.xlsx$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'data-cache',
+              networkTimeoutSeconds: 5,
+            }
+          },
+          {
+            urlPattern: /^https:\/\/(image\.tmdb\.org|tse\d\.mm\.bing\.net)\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'poster-cache',
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+              }
+            }
+          }
+        ]
       }
-    },
-    {
-      urlPattern: /^https:\/\/(image\.tmdb\.org|tse\d\.mm\.bing\.net)\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'poster-cache',
-        expiration: {
-          maxEntries: 500,
-          maxAgeSeconds: 60 * 60 * 24 * 30
-        }
-      }
-    }
-  ]
-}
     })
   ]
 })
